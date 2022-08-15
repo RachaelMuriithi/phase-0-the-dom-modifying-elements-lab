61a397f6faafcb48962d23ad13ee5cf98d0a084b
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-15 13:36:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement ("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textcontent = (i + 1 ).toString(); 
+//     ul.append(li);
+// }
+// element.append(ul);
